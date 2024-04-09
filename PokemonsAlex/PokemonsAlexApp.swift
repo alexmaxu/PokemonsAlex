657d570d378833b70849120febb9d36e3dab590a
alexmaxu <12alexmaxu@gmail.com>
2024-04-09 09:16:57 +0200

    Initial Commit

---
+++ PokemonsAlex/PokemonsAlexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonsAlexApp.swift
+//  PokemonsAlex
+//
+//  Created by Alex  on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonsAlexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
